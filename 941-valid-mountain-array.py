1b34bdfa1c0271a487c0d4d74b6e967831d0e366
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-01-19 16:05:17 +0300

    Create 941-valid-mountain-array.py

---
+++ 941-valid-mountain-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+        if n<3:
+            return False
+        pivot = max(arr)
+        ind = arr.index(pivot)
+        if ind == 0  or ind == n-1:
+            return False
+        for i in range(ind- 1):
+            if arr[i]>= arr[i+1]:
+                return False
+        for j in range(ind, n-1) :
+            if arr[j] <= arr[j+1]:
+                return False
+        return True
