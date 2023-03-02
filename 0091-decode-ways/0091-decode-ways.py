dbae477254c475fef56afd1313849d9f85f84f7c
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-02 16:22:39 +0300

    Time: 38 ms (41.82%), Space: 14.2 MB (16.01%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        @cache
+        def dp(i):
+            if i == len(s):
+                return 1
+            if s[i] == "0":
+                return 0
+            res = dp(i + 1)
+            if i + 1 < len(s):
+                if int(s[i : i + 2]) < 27:
+                    res += dp(i + 2)
+            return res
+        return dp(0)
