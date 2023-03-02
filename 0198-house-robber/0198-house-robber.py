e509cf69815546da605247b014a54121554a630e
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-02 15:24:18 +0300

    Time: 33 ms (67.17%), Space: 13.9 MB (56.10%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i >= len(nums):
+                return 0
+            return max(nums[i] + dp(i + 2),dp(i + 1))
+        return dp(0)
