0a009e4d9f18e1335acd79e46ad1e810af69f496
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-02 15:58:41 +0300

    Time: 34 ms (64.97%), Space: 13.9 MB (5.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -5,11 +5,11 @@ class Solution:
 #                 return i
 #             return dp(i-2)+ dp(i-1)
 #         return dp(n)
-        
+        if n == 0:
+            return 0
         dp = [0] * (n + 1)
-        for i in range(len(dp)):
-            if i == 0 or i == 1:
-                dp[i] =  i
-                continue
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,len(dp)):
             dp[i] =  dp[i-2]+ dp[i-1]
         return dp[n]
