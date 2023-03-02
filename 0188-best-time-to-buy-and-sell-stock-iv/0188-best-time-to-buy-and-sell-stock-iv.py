fe4265635fd7152df99ab0fa56479abdc92a5079
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-02 16:59:11 +0300

    Time: 371 ms (23.50%), Space: 89.6 MB (5.09%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self,k ,prices: List[int]) -> int:
+        @cache
+        def dp(i,canBuy,count):
+            if i == len(prices) or count == 0:
+                return 0
+            # can buy
+            if canBuy:
+                return max(dp(i + 1, False,count - 1) - prices[i],dp(i + 1, True,count))
+            else:
+                return max(prices[i] + dp(i + 1, True,count - 1),dp(i + 1, False, count))
+        return dp(0,True, k * 2)
+            
+            
