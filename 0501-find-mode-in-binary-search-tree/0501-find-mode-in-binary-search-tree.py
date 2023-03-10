80a7e77757cccd8a4be3eeb4e2b6fbd5f49a72e3
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-10 16:50:10 +0300

    Time: 58 ms (62.67%), Space: 18.3 MB (71.16%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,22 +6,18 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        self.count = Counter({})
-        
+        count = Counter({})
         def FM(root):
             if not root:
                 return 0
-            self.count[root.val] += 1
+            count[root.val] += 1
             FM(root.left)
             FM(root.right)
         FM(root)
         res = []
-        ls = list(sorted(self.count.items(), key=lambda item: item[1], reverse = True))
-        mx = max(self.count.values())
-        for i in range(len(ls)):
-            if ls[i][1] == mx:
-                res.append(ls[i][0])
-            else:
-                break
+        mx = max(count.values())
+        for ky, val in (count.items()):
+            if count[ky] == mx:
+                res.append(ky)
         return res
         
