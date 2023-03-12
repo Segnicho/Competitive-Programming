fdd5964099a2b3da6f861983b0d7a3ed79f6fe34
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-12 11:16:25 +0300

    Time: 28 ms (86.47%), Space: 13.9 MB (49.91%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -4,14 +4,11 @@ class Solution:
         def dp(i, arr):
             if i == len(arr) - 1:
                 return arr
-            else:
-                next_arr = [1]
-                for j in range(1, len(arr)):
-                    next_arr.append(arr[j]+arr[j-1])       
-                next_arr.append(1)
-                return dp(i, next_arr)
             
-        if rowIndex == 0:
-            return [1]
+            next_arr = [1]
+            for j in range(1, len(arr)):
+                next_arr.append(arr[j]+arr[j-1])       
+            next_arr.append(1)
+            return dp(i, next_arr)
         return dp(rowIndex, [1])
         
