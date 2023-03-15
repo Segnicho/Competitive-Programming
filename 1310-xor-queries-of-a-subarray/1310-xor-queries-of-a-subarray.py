5904018232b9e5c49ceccaf717b72502adbd57f6
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-15 18:17:37 +0300

    Time: 394 ms (65.33%), Space: 28.8 MB (83.94%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -1,14 +1,13 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
         
-        prefSum = [arr[0]]
+        res  = []        
         for i in range(1, len(arr)):
-            prefSum.append(prefSum[i-1] ^ arr[i])
-        res = []
+            arr[i] = arr[i-1] ^ arr[i]
         for l, r in queries:
             if l == 0:
-                res.append(prefSum[r])
+                res.append(arr[r])
             else:
-                res.append(prefSum[r] ^ prefSum[l-1])
+                res.append(arr[r] ^ arr[l-1])
         return res
         
