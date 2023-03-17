eef0e73bcff8a0038760e034aa60ae6ddab57958
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-17 16:33:46 +0300

    Time: 57 ms (66.80%), Space: 15.4 MB (53.13%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,16 +7,15 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         self.res = 0
-        counter = Counter({0:1})
-        def dp(root = root,sm = 0):
+        prefSum = Counter({0:1})
+        def dfs(root = root,currSum = 0):
             if not root:
                 return 0
-            sm += root.val
-            self.res += counter[sm-targetSum]
-            counter[sm] += 1
-            dp(root.left, sm )
-            dp(root.right, sm )
-            counter[sm] -= 1
-        dp()
+            currSum += root.val
+            self.res += prefSum[currSum-targetSum]
+            prefSum[currSum] += 1
+            dfs(root.left, currSum )
+            dfs(root.right, currSum )
+            prefSum[currSum] -= 1
+        dfs()
         return self.res
-    
