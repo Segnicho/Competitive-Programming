515217d1deeb1644dd5e11a7f3a4001e517bb841
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-18 18:39:28 +0300

    Time: 39 ms (26.83%), Space: 14.2 MB (15.93%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,11 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
         @cache
         def dp(i, n):
             if i >= n:
                 return 0
             return max(nums[i] + dp(i+2, n), dp(i+1, n) )
-        return max(dp(0, len(nums) -1), dp(1, len(nums)))
+        return max(nums[0], dp(0, len(nums) -1), dp(1, len(nums)))
     
