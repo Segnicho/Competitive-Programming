eb495978445039b4b48d65875aa7fa98c1c31aed
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-24 07:20:28 +0300

    Time: 2083 ms (16.16%), Space: 30.2 MB (24.19%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        def partition(arr, left, right):
+            l = left -1
+            pivot = arr[right]
+            for i in range(left, right):
+                if arr[i] <= pivot:
+                    l += 1
+                    arr[l] , arr[i] = arr[i], arr[l]
+            arr[l + 1], arr[right] = arr[right] , arr[l + 1]
+            return l + 1        
+        def quickSort(arr, left, right):
+            if left <= right:
+                ptr = partition(arr, left, right)
+                if ptr == len(arr) - k:
+                    return arr[ptr]
+                elif ptr < len(arr) - k:
+                    return quickSort(arr, ptr + 1, right)    
+                elif ptr > len(arr) - k:
+                    print(left, ptr-1)
+                    return quickSort(arr, left, ptr - 1)
+        d = quickSort(nums,0, len(nums) -1) 
+        return d
+    
