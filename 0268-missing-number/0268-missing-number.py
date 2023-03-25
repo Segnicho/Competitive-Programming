45c55881110126dcdd5aef1bf8a7836ac94d18cc
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-03-25 21:28:56 +0300

    Time: 162 ms (26.02%), Space: 15.1 MB (67.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums)+1):
-            if i not in nums:
+        for i in range(len(nums)):
+            while  nums[i] < len(nums) and nums[i] != i:
+                temp = nums[i]
+                nums[i] = nums[temp]
+                nums[temp] = temp
+        for i in range(len(nums)):
+            if i != nums[i]:
                 return i
-            
-        
+        return len(nums)
+    
