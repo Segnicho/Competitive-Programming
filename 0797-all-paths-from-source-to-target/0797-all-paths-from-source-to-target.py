21877723d63216005f6f921c045b15302ecf2633
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-04-12 18:46:05 +0300

    Time: 106 ms (39.57%), Space: 15.6 MB (96.64%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -6,21 +6,20 @@ class Solution:
         
         for i in range(len(graph)):
             adjList[i] = graph[i] 
-        self.res = []
+        res = []
+        
         def dfs(i, arr):
             arr.append(i)
             if i+1 == len(graph):
-                # print(arr, i)
-                self.res.append(arr[:])
+                res.append(arr[:])
                 return
             for number in adjList[i]:
                 dfs(number, arr)
                 arr.pop()
-            return arr
         for number in adjList[0]:
             arr = [0]
             dfs(number, arr)
-        return self.res
+        return res
     
     
     
