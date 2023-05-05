d6ede6c25cda275ca8cb255cc6298350f506226d
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-05 08:43:36 +0300

    Time: 78 ms (6.08%), Space: 16.3 MB (7.47%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,8 +1,8 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        res = []
+        heap = []
         count = Counter(words)
         for word, freq in count.items():
-            res.append((freq, word))
-        res.sort(key=lambda r: (-r[0], r[1]))
-        return [res[i][1] for i in range(k)]
+            heap.append((-freq, word))
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for i in range(k)]
