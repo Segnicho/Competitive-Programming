feb23107913d761fd5dd77dff1b8f9312f5b72f7
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-08 19:34:55 +0300

    Time: 114 ms (23.88%), Space: 19.6 MB (6.80%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,5 +1,52 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+        black = set()
+        gray = set()
+        stack = []
+        cycle =False
+        def dfs(node):
+            nonlocal cycle
+            if node in black:
+                return
+            if node in gray:
+                cycle = True
+                return
+            gray.add(node)
+            if node not in graph:
+                stack.append(node)
+                black.add(node)
+                return
+            for neigh in graph[node]:
+                dfs(neigh)
+            stack.append(node)            
+            black.add(node)
+        for i in range(numCourses):
+            dfs(i)
+
+        if cycle or len(stack) < numCourses:
+            return []
+        stack.reverse()
+        return stack
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         nums = set()
         for i, j in prerequisites:
             nums.add(i)
