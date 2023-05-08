6c67c5159509b85b0ff9d2f94a8adab9276f32bf
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-08 18:34:06 +0300

    Time: 151 ms (10.24%), Space: 18.2 MB (10.70%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        nums = set()
+        for i, j in prerequisites:
+            nums.add(i)
+            nums.add(j)
+            
+        res =[]
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+        
+        indegree = Counter()
+        
+        q = deque()
+        seen = set()
+        for ky, val in graph.items():
+            ky not in seen and q.append(ky) 
+            for num in val:
+                seen.add(num)
+                indegree[num] += 1
+                if num in q:
+                    q.remove(num)
+        for i in range(numCourses):
+            if i not in nums:
+                q.append(i)
+        # print(graph, q)
+        while q:
+            curr = q.popleft()
+            res.append(curr)
+            for num in graph[curr]:
+                if indegree[num] == 1:
+                    q.append(num)
+                indegree[num] -= 1
+        for ky, val in indegree.items():
+            if val > 0:
+                return []
+        return res
