1989f514bc9c9d36f79b4ba48b967f92e5fc4d53
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-11 19:40:26 +0300

    Time: 1971 ms (68.16%), Space: 51.4 MB (95.95%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -2,20 +2,19 @@ class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         heap = []
         res = []
-        for i, num in enumerate(tasks):
-            num.append(i)
-        tasks.sort(reverse=True)
         time = 0
+        for idx, task in enumerate(tasks):
+            task.append(idx)
+        tasks.sort(reverse = True)
+        index = 0
         while tasks:
-            # handle those that can be handled directly as they came and time suits
-            start, t, idx = tasks.pop()
+            start, t, index = tasks.pop()
             while heap and  start > time:
-                ct, cidx = heappop(heap)
-                res.append(cidx)
+                ct, idx = heappop(heap)
+                res.append(idx)
                 time += ct
-            heappush(heap, [t, idx])
+            heappush(heap, [t, index])
             time = max(time, start)
-        # handle those which are left from the first process
         while heap:
             t, idx = heappop(heap)
             res.append(idx)
