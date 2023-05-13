6578d0c434bfed2165bd2925d26046b66c8a999c
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-13 08:00:59 +0300

    Time: 106 ms (44.10%), Space: 17.9 MB (24.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,38 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-     
-        nums = set()
-        for i, j in prerequisites:
-            nums.add(i)
-            nums.add(j)
-            
-        res =[]
+        indegree = [0]*numCourses
         graph = defaultdict(list)
-        for course, prereq in prerequisites:
-            graph[prereq].append(course)
-        indegree = Counter()
-        
+        for u, v in prerequisites:
+            graph[v].append(u)
+            indegree[u] += 1
         q = deque()
-        seen = set()
-        for ky, val in graph.items():
-            ky not in seen and q.append(ky) 
-            for num in val:
-                seen.add(num)
-                indegree[num] += 1
-                if num in q:
-                    q.remove(num)
         for i in range(numCourses):
-            if i not in nums:
+            if indegree[i] == 0:
                 q.append(i)
+        print(q)
+        res = []
         while q:
-            curr = q.popleft()
-            res.append(curr)
-            for num in graph[curr]:
-                if indegree[num] == 1:
-                    q.append(num)
-                indegree[num] -= 1
-        for ky, val in indegree.items():
-            if val > 0:
-                return False
+            node = q.popleft()
+            res.append(node)
+            for neigh in graph[node]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        if len(res) != numCourses:
+            return 0
         return True
