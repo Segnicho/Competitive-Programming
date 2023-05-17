0addcf95f46c48faf3f72f261e332087f1890ff3
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-17 17:14:30 +0300

    Time: 2000 ms (55.85%), Space: 105.6 MB (84.33%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,21 +1,30 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
-        graph = defaultdict(set)
-        
-        for start, end in edges:
-            graph[start].add(end)
-            graph[end].add(start)
-        
-        def dfs(node, visited):
-            if node == destination:
-                return True
-            visited.add(node)
-            for neighbour in graph[node]:
-                if neighbour not in visited:
-                    found = dfs(neighbour, visited)
-                    if found:
-                        return True                        
-            return False
-        return dfs(source, set())
+        uf = UnionFind(n)
+        for u, v in edges:
+            uf.union(u, v)
+        return uf.isConnected(source, destination)
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1]*size
     
+    def find(self, u):
+        if self.root[u] == u:
+            return u
+        self.root[u] = self.find(self.root[u])
+        return self.root[u]
+    
+    def union(self, u, v):
+        rootu = self.find(u)
+        rootv = self.find(v)
+        if self.rank[rootu] > self.rank[rootv]:
+            self.root[rootv] = rootu
+        elif self.rank[rootv] > self.rank[rootu]:
+            self.root[rootu] = rootv
+        else:
+            self.root[rootu] = rootv
+            self.rank[rootv] += 1
+    def isConnected(self, u, v):
+        return self.find(u) == self.find(v)
