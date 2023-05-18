6ab33420febbb67d1f96c25e1cae6df675e050c2
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-18 14:31:49 +0300

    Time: 95 ms (50.70%), Space: 18 MB (19.21%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 0
+        for i in range(len(nums)):
+            if nums[left] != nums[i]:
+                left += 1
+                nums[left] = nums[i]
+        return left+1
