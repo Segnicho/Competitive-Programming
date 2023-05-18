1eaea2f2bcfbd7dcb4ff5994710323c297bef7a6
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-18 14:42:15 +0300

    Time: 990 ms (74.06%), Space: 27.3 MB (21.23%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @cache
-        def dp(i,canBuy,count):
-            if i == len(prices) or count == 0:
-                return 0
-            # can buy
-            if canBuy:
-                return max(dp(i + 1, False,count - 1) - prices[i],dp(i + 1, True,count))
+        mn = float("inf")
+        res = 0
+        for i in range(len(prices)):
+            if prices[i] < mn:
+                mn = prices[i]
             else:
-                return max(prices[i] + dp(i + 1, True,count - 1),dp(i + 1, False, count))
-        return dp(0,True, 2 )
+                res = max(res, prices[i] - mn)
+        
+        return res
