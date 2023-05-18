a1f19f477b892d17c2bf758c7b701dbcad191eeb
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-18 21:49:36 +0300

    Time: 138 ms (26.07%), Space: 20 MB (25.36%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,19 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        res = []
-        for ls in lists:
-            node = ls
-            while node:
-                res.append(node.val)
-                node = node.next
-        heapify(res)
-        ans = []
-        node = None
-        if res:
-            node = ListNode(heappop(res))
-        dummy = node
-        while res:
-            node.next = ListNode(heappop(res))            
-            node = node.next
-        return dummy
+        
+        def mergeTwoLists(n1, n2):
+            tail = dummy = ListNode()
+            while n1 and n2:
+                if n1.val < n2.val:
+                    tail.next = n1
+                    n1 = n1.next
+                else:
+                    tail.next = n2
+                    n2 = n2.next
+                tail = tail.next
+            tail.next = n1 or n2
+            return dummy.next
+        
+        if not lists:
+            return None
+        n = len(lists)
+        while n > 1:
+            k = math.ceil(n/2)
+            for i in range(n//2):
+                lists[i] = mergeTwoLists(lists[i], lists[i+k])
+            n = k
+        return lists[0]
