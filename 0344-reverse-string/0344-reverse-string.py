8f5dde9d31706a016f62a14944fb246718425659
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-19 10:22:03 +0300

    Time: 224 ms (18.95%), Space: 20.7 MB (31.20%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,11 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
-        def dfs(l, r):
-        
-            if l >= r :
-                return 
-            s[l],s[r] = s[r], s[l]
-            dfs(l+1, r-1)
-        dfs(0, len(s)-1)
+        left = 0 
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
+        return s
