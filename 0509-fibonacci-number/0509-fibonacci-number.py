bdd81c81360fdbbeaad82f75b6f4a78151ae04dd
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-19 10:08:47 +0300

    Time: 43 ms (48.65%), Space: 16.4 MB (16.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,17 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-#         def dp(i):
-#             if i == 0 or i == 1:
-#                 return i
-#             return dp(i-2)+ dp(i-1)
-#         return dp(n)
-        if n == 0:
-            return 0
-        dp = [0] * (n + 1)
-        first = 0
-        second = 1
-        for i in range(2,len(dp)):
-            temp = second
-            second =  first + second
-            first = temp
-        return second
+        fseq = [0, 1, 1]
+        for i in range(n-2):
+            fseq.append(fseq[i+1] + fseq[i+2])
+        return fseq[n]
