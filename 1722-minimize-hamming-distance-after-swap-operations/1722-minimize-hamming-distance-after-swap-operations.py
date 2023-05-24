d9ec2de441f4d9fe8965e8d805fa2be15f3421c7
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-24 17:26:51 +0300

    Time: 1630 ms (46.87%), Space: 62.7 MB (66.67%) - LeetHub

--- 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.py
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.py
@@ -6,41 +6,20 @@ class Solution:
         for i, j in allowedSwaps:
             uf.union(i,j)
         
-        hm = defaultdict(list)
+        counter = defaultdict(list)
         
         for i in range(m):
-            hm[uf.find(i)].append(i)
-        
+            counter[uf.find(i)].append(i)
         total = 0
-        
-        for x in hm.values():
+        for x in counter.values():
             s = Counter()
             t = Counter()
-            
             for i in x:
                 s[source[i]] += 1
                 t[target[i]] += 1
-        
             final = s & t
             total += sum(final.values())
         return m - total
-        
-#         parent = defaultdict(int)
-#         children = defaultdict(set)
-        
-#         for i in range(m):
-#             root = uf.find(i)
-#             children[root].add(source[i])
-#             parent[i] = root
-#         count = Counter(source)
-#         cnt = 0
-#         for i in range(m):            
-#             if (target[i] != source[i] and target[i] not in children[parent[i]]) or ((target[i] ==source[i] or target[i] in children[parent[i]]) and count[target[i]] <= 0) :
-#                 cnt += 1
-#             if target[i] ==source[i] or target[i] in children[parent[i]]:
-#                 count[target[i]] -= 1
-#         return cnt
-        
         
 class UnionFind:
     def __init__(self, size):
