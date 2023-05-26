0451198a10d13372640e0c7998600b8f1d5a4d58
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-26 18:19:51 +0300

    Time: 47 ms (24.14%), Space: 16.3 MB (8.13%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @cache
+        # @cache
+        dikt = defaultdict(int)
         def dp(i):
             if i == 1:
                 return 1
             elif  i == 2:
                 return 2
-            return dp(i-2) + dp(i-1)
+            if dikt[i]:
+                return dikt[i]
+            res = dp(i-2) + dp(i-1)
+            dikt[i] = res 
+            return res
         return dp(n)
+    
+    
