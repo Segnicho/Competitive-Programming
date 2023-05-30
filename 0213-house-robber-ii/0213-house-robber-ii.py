7d9f4c4a1d5aa59c7e0c9692b903e9b3b69b6dbf
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-30 19:16:41 +0300

    Time: 51 ms (21.25%), Space: 16.5 MB (17.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @cache
-        def dp(i, n):
-            if i >= n:
+        n = len(nums)
+        @lru_cache(maxsize = None)
+        def dp(i, size):
+            if i >=size:
                 return 0
-            return max(nums[i] + dp(i+2, n), dp(i+1, n) )
-        return max(nums[0], dp(0, len(nums) -1), dp(1, len(nums)))
-    
+            return max(nums[i]+dp(i+2, size), dp(i+1, size))
+        return max(nums[0], dp(0, n-1), dp(1, n)) 
