f1598e505770eaa7322db5efb68f5817454f97ab
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-05-30 18:58:59 +0300

    Time: 279 ms (67.80%), Space: 44.6 MB (5.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        count = 0
         n = len(nums)
         memo = defaultdict(int)
+        @lru_cache(maxsize = None)
         def dp(i, targetSum):
             if i >= n:
                 if targetSum == target:
                     return 1
                 return 0            
-            state = (targetSum, i)
-            if state not in memo:
-                memo[state] = dp(i+ 1,  targetSum + nums[i]) + dp(i + 1 , targetSum - nums[i]) 
-            return memo[state]
+    
+            return dp(i+ 1,  targetSum + nums[i]) + dp(i + 1 , targetSum - nums[i]) 
+
         return dp(0, 0)
     
