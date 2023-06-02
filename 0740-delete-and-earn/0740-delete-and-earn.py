776b7c47dc0bfe3492034b5e6ab58d3b63612d51
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-06-02 09:23:05 +0300

    Time: 99 ms (47.49%), Space: 23.6 MB (19.93%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -8,11 +8,15 @@ class Solution:
             if nums[i] > uniques[-1]:
                 uniques.append(nums[i])
         m = len(uniques)
-        @cache
         def dp(i):
             if i >= m:
                 return 0
+            if memo[i]:
+                return memo[i]
             if i+1 < m and uniques[i+1] == uniques[i] + 1:
-                return max(count[uniques[i]]*uniques[i]+ dp(i+2), dp(i+1))
-            return count[uniques[i]]*uniques[i] +  dp(i+1) 
+                memo[i] = max(count[uniques[i]]*uniques[i]+ dp(i+2), dp(i+1))
+                return memo[i]
+            memo[i] = count[uniques[i]]*uniques[i] +  dp(i+1)
+            return memo[i]
+        memo = defaultdict(int)
         return dp(0)
