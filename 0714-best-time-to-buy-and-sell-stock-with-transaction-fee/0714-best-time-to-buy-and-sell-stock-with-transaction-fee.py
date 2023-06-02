801ade7e9e7266adbb655c8e0b4a00cde4a0a280
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-06-02 21:16:50 +0300

    Time: 1410 ms (17.25%), Space: 258.8 MB (5.16%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,11 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         @cache
-        def dp(i, canSell):
+        def dp(i, canBuy):
             if i >= len(prices):
                 return 0
-            if canSell:
-                return max(prices[i] - fee + dp(i+1, False), dp(i+1, True))
+            if canBuy:
+                return max(-prices[i] + dp(i+1, False), dp(i+1, True))                
             else:
-                return max(-prices[i] + dp(i+1, True), dp(i+1, False))                
-        return dp(0, False)
+                return max(prices[i] - fee + dp(i+1, True), dp(i+1, False))
+        return dp(0, True)
+    
