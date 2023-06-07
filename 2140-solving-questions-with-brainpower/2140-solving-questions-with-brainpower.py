e00fb816a94dd5be83d1f8c675c9179ce0955bac
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-06-07 19:29:32 +0300

    Time: 1778 ms (19.05%), Space: 197.3 MB (6.26%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        
+        @cache
+        def dp(i):
+            if i  >= len(questions):
+                return 0
+            return max(questions[i][0]+ dp(i+questions[i][1] + 1), dp(i+1))
+        
+        return dp(0)
+            
