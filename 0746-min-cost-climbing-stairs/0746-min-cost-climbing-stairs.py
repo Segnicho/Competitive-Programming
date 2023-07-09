0cf1ee70ef4b5d1ec5bc452f1317fb7460f7c883
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-07-09 09:59:12 +0300

    Time: 84 ms (10.13%), Space: 18.9 MB (6.10%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,10 +1,10 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        @cache
         def dp(i):
-            if i >= n:
+            if i < 2:
                 return 0
-            return cost[i] + min(dp(i+2) , dp(i+1))
-        return min(dp(0), dp(1))
-    
+            if i not in memo:
+                memo[i] = min(cost[i-1]+ dp(i-1),cost[i-2]+ dp(i-2))
+            return memo[i]
+        memo = {}
+        return dp(len(cost))
