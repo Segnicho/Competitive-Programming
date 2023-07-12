f16e830c2e6c92aad052a40146eb0520b6858140
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-07-12 20:16:50 +0300

    Time: 49 ms (88.57%), Space: 16.5 MB (73.23%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         res = []
-        def backTrack(arr,temp):
-            if not arr:
-                res.append(temp[:])
+        if n == 1:
+            return [nums[:]]
+        for i in range(n):
+            num = nums.pop(0)
+            perms = self.permute(nums)
+            for perm in perms:
+                perm.append(num)
+                res.append(perm)
+            nums.append(num)
             
-            for num in arr:
-                temp.append(num)
-                otherNums = arr[:]
-                otherNums.remove(num)                
-                backTrack(otherNums, temp)
-                temp.pop()
-        backTrack(nums, [])
-        
         return res
-    
