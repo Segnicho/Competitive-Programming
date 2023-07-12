0195d8a6dc3b5d162d744d70926aba683855146f
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-07-12 12:01:07 +0300

    Time: 1791 ms (13.13%), Space: 62.5 MB (80.81%) - LeetHub

--- 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.py
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.py
@@ -1,45 +1,39 @@
 class Solution:
     def minimumHammingDistance(self, source: List[int], target: List[int], allowedSwaps: List[List[int]]) -> int:
-        m = len(source)
-        uf = UnionFind(m)
-        
+        n = len(source)
+        uf = UnionFind(n)
+        def getHD(arr):
+            s1 =Counter(source[i] for i in arr)
+            s2 =Counter(target[i] for i in arr)
+            return sum(val for val in (s1-s2).values())
         for i, j in allowedSwaps:
-            uf.union(i,j)
-        
-        counter = defaultdict(list)
-        
-        for i in range(m):
-            counter[uf.find(i)].append(i)
-        total = 0
-        for x in counter.values():
-            s = Counter()
-            t = Counter()
-            for i in x:
-                s[source[i]] += 1
-                t[target[i]] += 1
-            final = s & t
-            total += sum(final.values())
-        return m - total
-        
+            uf.union(i, j)
+        group = defaultdict(list)
+        for i in range(n):
+            group[uf.find(i)].append(i)
+        return sum(getHD(v) for v in group.values())
+            
 class UnionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
-        self.rank = [1] * size
-
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
+        self.rank = [1]*size
+    def find(self, u):
+        if self.root[u] == u:
+            return u
+        self.root[u] = self.find(self.root[u])
+        return self.root[u]
+    
+    def union(self, u, v):
+        rootu = self.find(u)
+        rootv = self.find(v)
+        if rootu == rootv:
+            self.ans = [u+1, v+1]
+        if rootu != rootv:
+            if self.rank[rootu] > self.rank[rootv]:
+                self.root[rootv] = rootu
+            elif self.rank[rootv] > self.rank[rootu]:
+                self.root[rootu] = rootv
             else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
+                self.root[rootu] = rootv
+                self.rank[rootv] += 1
+        
