1817b50e17d59be1fd9df6db719bbb0e5028677a
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-07-12 16:33:37 +0300

    Time: 46 ms (73.30%), Space: 16.7 MB (11.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,8 +7,7 @@ class Solution:
             if s[i] == "0":
                 return 0
             res = dp(i + 1)
-            if i + 1 < len(s):
-                if int(s[i : i + 2]) < 27:
+            if i + 1 < len(s) and int(s[i : i + 2]) < 27:
                     res += dp(i + 2)
             return res
         return dp(0)
