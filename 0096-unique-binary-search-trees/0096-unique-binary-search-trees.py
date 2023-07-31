42f789f6e415a6a34947abfa0e5a45f1977f268e
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-07-31 15:39:34 +0300

    Time: 34 ms (97.33%), Space: 16.3 MB (33.00%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,22 +1,9 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        
-        dp = [0] * (n+1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(2, n+1):
-            for j in range(i):
-                dp[i] += dp[j] * dp[i-j-1]
-        return dp[n]
-        
-        @cache
-        def findFact(num):
-            if num <= 1:
-                return 1
-            return num*findFact(num-1)
-        return findFact(2*n)//(findFact(n+1) * findFact(n))
+        G = [0] * (n + 1)
+        G[0] = G[1] = 1
+
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                G[i] += G[j - 1] * G[i - j]
+        return G[n]
