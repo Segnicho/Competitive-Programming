212a9919b2bbb9d209739e8cadaaddf0852925cc
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-08-23 22:12:35 +0300

    Time: 148 ms (5.17%), Space: 16.3 MB (79.19%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,21 +1,23 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
         res = []
-        def backTracking(i, temp, sm):
-            if sm == target:
-                res.append(temp[:])
+        visited = []
+        def rec(i=0, temp=[], sm=0):
+            if i == n:
+                if sm == target:
+                    res.append(temp[:])
                 return
-            ans = set()
-            for index in range(i, len(candidates)): 
-                if candidates[index] not in ans:
-                    ans.add(candidates[index])
-                    if sm + candidates[index] > target :
-                        break
-                    temp.append(candidates[index])
-                    sm += candidates[index]
-                    backTracking(index+1, temp, sm)
-                    x = temp.pop()
-                    sm -= x
-        candidates.sort()
-        backTracking(0, [] , 0)
+            if sm > target:
+                return
+            temp.append(nums[i])
+            visited.append(nums[i])
+            sm += nums[i]
+            rec(i+1, temp, sm)
+            visited.pop()
+            sm -= temp.pop()
+            if nums[i] not in visited:
+                rec(i+1, temp, sm)
+        nums.sort()
+        rec()
         return res
