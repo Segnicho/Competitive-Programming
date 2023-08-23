4a397c29582de31a317ae5d7dbce1e95da34e54b
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-08-23 20:22:55 +0300

    Time: 60 ms (80.41%), Space: 16.5 MB (54.93%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,18 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
         res = []
-        def recur(i, arr):
-            if target == sum(arr):
-                res.append(arr[:])
+        def dfs(i=0, sm=0, temp =  []):
+            
+            if sm == target:
+                res.append(temp[:])
                 return
-            elif sum(arr) > target:
+            if sm > target:
                 return
-            for j in range(i,  len(candidates)):
-                arr.append(candidates[j])
-                recur(j, arr)
-                arr.pop()                
-            return arr
-        recur(0, [])
+            for j in range(i, len(nums)):
+                sm += nums[j]
+                temp.append(nums[j])
+                dfs(j, sm, temp)
+                sm -= temp[-1]
+                temp.pop()
+        dfs()
         return res
