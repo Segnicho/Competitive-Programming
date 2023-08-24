a4ff16ffec96a588f99586630a6a91d53356a135
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-08-24 08:23:00 +0300

    Time: 68 ms (76.06%), Space: 16.5 MB (49.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -3,21 +3,31 @@ class Solution:
         n = len(nums)
         res = []
         visited = []
-        def rec(i=0, temp=[], sm=0):
-            if i == n:
-                if sm == target:
-                    res.append(temp[:])
-                return
+        def rec(ind=0, temp=[], sm=0):
             if sm > target:
                 return
-            temp.append(nums[i])
-            visited.append(nums[i])
-            sm += nums[i]
-            rec(i+1, temp, sm)
-            visited.pop()
-            sm -= temp.pop()
-            if nums[i] not in visited:
+            if sm == target:
+                res.append(temp[:])
+                return
+            for i in range(ind, n):
+                if i > ind and nums[i] == nums[i-1]: 
+                    continue
+                
+                temp.append(nums[i])
+                sm += nums[i]
                 rec(i+1, temp, sm)
+                sm -= temp.pop()
+                
+            # if sm > target:
+            #     return
+            # temp.append(nums[i])
+            # visited.append(nums[i])
+            # sm += nums[i]
+            # rec(i+1, temp, sm)
+            # visited.pop()
+            # sm -= temp.pop()
+            # if nums[i] not in visited:
+            #     rec(i+1, temp, sm)
         nums.sort()
         rec()
         return res
