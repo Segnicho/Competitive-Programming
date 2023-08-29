e5eaf8de6f6fe12c367a65e0e290e895ba2a871e
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-08-29 15:33:22 +0300

    Time: 449 ms (83.07%), Space: 29.8 MB (15.34%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,34 +1,33 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-        indegree = defaultdict(int)
         if n == 1:
             return [0]
-        for start, end in edges:
-            graph[start].append(end)    
-            graph[end].append(start)
-            indegree[start] += 1
-            indegree[end] += 1
-            
-        q = deque()
-        for ky, val in graph.items():
-            if len(val) == 1:
-                q.append(ky)
-        cnt = 0
+        
+        graph = defaultdict(list)
+        indegree = [0]*n
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+            indegree[i] += 1
+            indegree[j] += 1
+        level = []
         visited = set()
-        while q and n-cnt > 2:
-            # print(q)
-            for _ in range(len(q)):
-                cnt += 1
-                node = q.popleft()
+        for num in range(n):
+            if indegree[num] == 1:
+                level.append(num)
+                visited.add(num)
+        res = level[:]
+        while level:
+            newLevel = []
+            for i in range(len(level)):
+                node = level.pop()
                 visited.add(node)
                 for neigh in graph[node]:
-                    indegree[neigh] -= 1
-                    if neigh not in visited and indegree[neigh] == 1:
-                        q.append(neigh)
-        res = []
-        for i in range(n):
-            if i not in visited:
-                res.append(i)
-        return q
-        
+                    if neigh not in visited:
+                        indegree[neigh] -= 1
+                        if indegree[neigh] == 1:
+                            newLevel.append(neigh)
+            if len(newLevel) > 0:
+                res = newLevel[:]
+            level = newLevel            
+        return res
