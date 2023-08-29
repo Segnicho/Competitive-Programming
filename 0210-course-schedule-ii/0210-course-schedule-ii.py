4f3a3d70c98ddacdff6123fcf26f17ba6428de72
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-08-29 14:46:38 +0300

    Time: 102 ms (70.60%), Space: 18 MB (73.37%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,85 +1,23 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        indegree = [0]*numCourses
         graph = defaultdict(list)
-        for course, prereq in prerequisites:
-            graph[prereq].append(course)
-        black = set()
-        gray = set()
-        stack = []
-        cycle =False
-        def dfs(node):
-            nonlocal cycle
-            if node in black:
-                return
-            if node in gray:
-                cycle = True
-                return
-            gray.add(node)
-            if node not in graph:
-                stack.append(node)
-                black.add(node)
-                return
-            for neigh in graph[node]:
-                dfs(neigh)
-            stack.append(node)            
-            black.add(node)
-        for i in range(numCourses):
-            dfs(i)
-
-        if cycle or len(stack) < numCourses:
-            return []
-        stack.reverse()
-        return stack
-            
-            
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        nums = set()
-        for i, j in prerequisites:
-            nums.add(i)
-            nums.add(j)
-            
-        res =[]
-        graph = defaultdict(list)
-        for course, prereq in prerequisites:
-            graph[prereq].append(course)
-        
-        indegree = Counter()
-        
+        res = []
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
         q = deque()
-        seen = set()
-        for ky, val in graph.items():
-            ky not in seen and q.append(ky) 
-            for num in val:
-                seen.add(num)
-                indegree[num] += 1
-                if num in q:
-                    q.remove(num)
-        for i in range(numCourses):
-            if i not in nums:
-                q.append(i)
-        # print(graph, q)
+        for num in range(numCourses):
+            if indegree[num] == 0:
+                q.append(num)
+                
         while q:
-            curr = q.popleft()
-            res.append(curr)
-            for num in graph[curr]:
-                if indegree[num] == 1:
-                    q.append(num)
-                indegree[num] -= 1
-        for ky, val in indegree.items():
-            if val > 0:
-                return []
-        return res
+            node = q.popleft()
+            res.append(node)
+            numCourses -= 1
+            for neigh in graph[node]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        return res if numCourses == 0 else []
