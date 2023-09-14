d5effec450413880db4e39561145a36a0a87f321
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-09-14 10:34:10 +0300

    Time: 44 ms (27.26%), Space: 16.6 MB (6.57%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,18 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        res = [[0 for _ in range(n)] for i in range(m)]
-        res[0] = [1]*n
-        for i in range(m):
-            res[i][0] = 1
-        for i in range(1, m):
-            for j in range(1, n):
-                res[i][j] =  res[i-1][j] + res[i][j-1]
-        return res[m-1][n-1]
+        
+        mp = defaultdict(int)
+        def dp(i, j):
+            if mp[(i, j)]:
+                return mp[(i,j)]
+            if i == m-1 and j == n-1:
+                return 1
+            if i >=m or j >=n:
+                return 0
+            down  = dp(i+1, j)
+            right  = dp(i, j+1)
+            res = down + right
+            mp[(i, j)]  =res
+            return res
+               
+        return dp(0, 0)
