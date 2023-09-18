ee4c17afeb309ed0b73936d02e3f6a2656a169e8
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-09-18 10:55:39 +0300

    Time: 36 ms (94.23%), Space: 16.4 MB (59.42%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,9 +1,12 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-
-        def recur(num = n):
-            for i in range(2, int(num**0.5)+1):
+        
+        def recur(num):
+            for i in range(2, int(sqrt(num))+1):
                 if num%i == 0:
-                    return recur(num/i) + i
-            return num
-        return int(recur()) if n != 1 else 0
+                    return int(recur(num/i)) + i 
+            return int(num)
+        if n == 1:
+            return 0
+        return recur(n)
+        
