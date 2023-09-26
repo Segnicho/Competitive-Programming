d992338ff5876f574fe7cc6fe8c84dfd0c977597
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-09-26 18:07:24 +0300

    Time: 33 ms (90.37%), Space: 31.4 MB (5.40%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        m = len(s)
+        n = len(t)
+        @cache
+        def dp(i, j):
+            if i >= m:
+                return True
+            if j >= n and i < m:
+                return False
+            if s[i]== t[j]:
+                return dp(i+1, j+1)
+            else:
+                return dp(i, j+1)
+        return dp(0, 0)
