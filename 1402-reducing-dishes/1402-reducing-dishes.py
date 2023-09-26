6b2b1847b21f695d7b69b2f8eeffbc3189575b5c
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-09-26 09:47:21 +0300

    Time: 44 ms (82.72%), Space: 16.2 MB (96.80%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        
-        satisfaction.sort(reverse=True)
-        total_satisfaction = max_satisfaction = 0
         n = len(satisfaction)
-        i = 0
-        while i<n and satisfaction[i] + total_satisfaction > 0:
-                total_satisfaction += satisfaction[i]
-                max_satisfaction += total_satisfaction
-                i+= 1
-        return max_satisfaction
+        satisfaction.sort(reverse=True)
+        res, curr, i = 0, 0, 0
+        while i < n and curr + satisfaction[i] > 0:
+            curr += satisfaction[i]
+            res += curr
+            i += 1
+        return res
