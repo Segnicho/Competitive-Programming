d777a5a3371182a5ab2ba88ad2073ae70236a190
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-09-29 17:56:42 +0300

    Time: 158 ms (24.20%), Space: 17.7 MB (28.27%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -6,16 +6,7 @@ class Solution:
         words.sort()
         for word in words:
             trie.insert(word)
-                
-#         def helper(i, word):
-#             if i <= 0:
-#                 return True
-#             start = trie.startsWith(word[:i])
-#             if not start:
-#                 return False
-#             if start and helper(i-1, word):
-#                 return True
-            
+         
         for word in words:
             isValid = trie.isValid(word)
             if isValid and len(word) > len(res):
@@ -27,7 +18,6 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.isEnd = False
-        self.count = 0
         
 class Trie:
     def __init__(self):
@@ -38,18 +28,8 @@ class Trie:
             if  not ch in curr.children:
                 nwNode = TrieNode()
                 curr.children[ch] = nwNode
-                curr.count = 1
-            else:
-                curr.count += 1
             curr = curr.children[ch]
         curr.isEnd = True
-    def startsWith(self, prefix: str) -> bool:
-        curr = self.root
-        for ch in prefix:
-            if not ch in  curr.children:
-                return False
-            curr = curr.children[ch]
-        return True
     def isValid(self, word):
         curr = self.root
         for ch in word:
@@ -57,4 +37,3 @@ class Trie:
                 return False
             curr = curr.children[ch]            
         return True
-# ["yo","ew","fc","zrc","yodn","fcm","qm","qmo","fcmz","z","ewq","yod","ewqz","y"]
