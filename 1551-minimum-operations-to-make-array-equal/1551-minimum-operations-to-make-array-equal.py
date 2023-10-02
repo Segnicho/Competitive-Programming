8ffad513dd3532524df99f435ff0199035ae22d5
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-02 18:44:33 +0300

    Time: 176 ms (5.35%), Space: 16.3 MB (61.78%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,13 +1,15 @@
 class Solution:
     def minOperations(self, n: int) -> int:
         arr = []
+        tot = 0
         for i in range(n):
-            arr.append(i*2 +1)
+            tot += (i*2 +1)
         
-        avg = sum(arr)/n
+        avg = tot/n
         ops = 0
         
-        for num in arr:
+        for i in range(n):
+            num = i*2 +1
             ops += abs(num-avg)
         
         return int(ops/2)
