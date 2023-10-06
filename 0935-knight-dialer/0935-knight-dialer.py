5807b55985b35adb667b91ab5dce089b1636e5eb
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-06 11:28:01 +0300

    Time: 5120 ms (11.76%), Space: 42.7 MB (31.51%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -6,20 +6,23 @@ class Solution:
             return nums or (x, y) == (3, 1)
         
         directions = [[2, 1], [2, -1], [-2, 1], [-2, -1], [-1, 2], [-1, -2], [1, 2] , [1, -2]]
-        @cache
         def dp(i,j, jump):
             if jump == 0:
                 return 1
             ans = 0
+            if (i, j, jump) in memo:
+                return memo[(i, j, jump)]
             for x, y in directions:
                 row, col = i+x, y+j
                 if inbound(row, col):
                     ans += dp(row, col, jump-1)
-            return ans%mod
+            memo[(i, j, jump)] =  ans % mod
+            return memo[(i, j, jump)] 
+
         res = 0
+        memo = {}
         for i in range(4):
             for j in range(3):
                 if inbound(i, j):
                     res += dp(i, j, n-1)
         return res % mod
-    
