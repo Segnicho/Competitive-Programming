e4a452e39fc10c8f02381559e3420810207439ea
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-10 06:28:16 +0300

    Time: 99 ms (35.46%), Space: 16.3 MB (71.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,14 +2,19 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         s1count  = Counter(s1)
         s2count  = Counter()
+        
         for right in range(len(s1)):
             if right < len(s2):
                 s2count[s2[right]] += 1
+        
         left = 0
+        
         for r in range(right+1, len(s2)):
-            if s1count == s2count:
+            if s1count == s2count:                
                 return True
             s2count[s2[left]] -= 1
             s2count[s2[r]] += 1
             left+=1
+            
+        
         return s1count == s2count
