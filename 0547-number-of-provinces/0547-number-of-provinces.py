881d945881ea63db267ff400637d1145134f29ae
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-14 07:24:26 +0300

    Time: 206 ms (65.70%), Space: 17.3 MB (40.45%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,40 +1,24 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        adjList = defaultdict(list)
         n = len(isConnected)
-        uf = UnionFind(n)
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i+1,n):
                 if isConnected[i][j]:
-                    uf.union(i, j)
-        return uf.getComponents()
+                    adjList[i].append(j)
+                    adjList[j].append(i)  
         
-        
-class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1]*size
-        self.components = 0
+        visited = [0]*(n)     
+        components = 0
+        for node in range(n):
+            if not visited[node]:
+                components += 1
+                self.dfs(node, -1, visited, adjList)
+            
+        return components     
     
-    def find(self, u):
-        if self.root[u] == u:
-            return u
-        self.root[u] = self.find(self.root[u])
-        return self.root[u]
-    
-    def union(self, u, v):
-        rootu = self.find(u)
-        rootv = self.find(v)
-        if self.rank[rootu] > self.rank[rootv]:
-            self.root[rootv] = rootu
-        elif self.rank[rootv] > self.rank[rootu]:
-            self.root[rootu] = rootv
-        else:
-            self.root[rootu] = rootv
-            self.rank[rootv] += 1
-    def isConnected(self, u, v):
-        return self.find(u) == self.find(v)
-    def getComponents(self):
-        for  i,num in enumerate(self.root):
-            if i == num:
-                self.components += 1
-        return self.components
+    def dfs(self,node, parent, visited, adjList):
+        visited[node] += 1
+        for neigh in adjList[node]:
+            if not visited[neigh] and neigh != parent:
+                self.dfs(neigh,node, visited, adjList)
