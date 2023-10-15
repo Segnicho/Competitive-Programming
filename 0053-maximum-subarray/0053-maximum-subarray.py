0efd68ac50713d556f958ea5ab3178e607333840
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-15 13:48:13 +0300

    Time: 624 ms (27.30%), Space: 30.5 MB (28.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,13 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        memo = {}
-        @cache
-        def dp(i, pick):
-            if i >= n:
-                return 0 if pick else -math.inf
-            if pick:
-                return max(0, nums[i]+ dp(i+1, True))
-            return max(dp(i+1, False), nums[i]+ dp(i+1, True))
-
-        return dp(0, False)
+        dp = [nums[0]]*n
+        
+        for i in range(1, n):
+            dp[i] = max(dp[i-1]+ nums[i] , nums[i])
+        
+        return max(dp)
