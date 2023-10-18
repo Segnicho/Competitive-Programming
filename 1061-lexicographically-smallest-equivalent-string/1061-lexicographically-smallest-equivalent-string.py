939da8249b99fa7f48733e8d51ab29ef276a8d13
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-18 06:58:26 +0300

    Time: 62 ms (13.43%), Space: 16.2 MB (96.02%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,42 +1,43 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        uf = UnionFind(26)
-        for i in range(len(s1)):
-            uf.union(ord(s1[i])-ord("a"), ord(s2[i])-ord("a"))
-        res = []
-        for ch in baseStr:
+        n = len(s1)
+        uf = UnionFind(n)
+        
+        for i in range(n):
+            c1 = ord(s1[i]) - ord("a")
+            c2 = ord(s2[i]) - ord("a")
+            uf.union(c1, c2)
+        
+        parent = (uf.parent)
+        res = ""
+        for c in baseStr:
+            root = uf.find(ord(c)-ord("a"))
             for i in range(26):
-                if uf.find(i) == uf.find(ord(ch)-ord("a")):
-                    res.append(chr(i+ord("a")))     
+                connected = uf.find(i)
+                if connected == root:
+                    res += chr(ord("a")+i)
                     break
-        return "".join(res)
+        return res
         
-        
-
 class UnionFind:
     def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.rank = [1]*size
-        self.ans = []
+        self.parent = [i for i in range(26)]
+        self.rank  = [1 for i in range(26)]
+    
     def find(self, u):
-        if self.root[u] == u:
-            return u
-        self.root[u] = self.find(self.root[u])
-        return self.root[u]
+        while u != self.parent[u]:
+            self.parent[u] = self.parent[self.parent[u]]
+            u = self.parent[u]
+        return self.parent[u]
     
     def union(self, u, v):
-        rootu = self.find(u)
-        rootv = self.find(v)
-        if rootu == rootv:
-            self.ans = [u+1, v+1]
-        if rootu != rootv:
-            if self.rank[rootu] > self.rank[rootv]:
-                self.root[rootv] = rootu
-            elif self.rank[rootv] > self.rank[rootu]:
-                self.root[rootu] = rootv
-            else:
-                self.root[rootu] = rootv
-                self.rank[rootv] += 1
-    def isConnected(self, u, v):
-        return self.find(u) == self.find(v)        
+        parentu = self.find(u)
+        parentv = self.find(v)
         
+        if self.rank[parentu] < self.rank[parentv]:
+            self.parent[parentu] = parentv
+            self.rank[parentu] += self.rank[parentv]
+        else:
+            self.parent[parentu] = parentv
+            self.rank[parentu] += self.rank[parentv]
+            
