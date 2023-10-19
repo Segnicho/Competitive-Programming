00d441348bb9d78c42c88994b71c87d80d33c32c
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2023-10-19 10:10:21 +0300

    Time: 3040 ms (34.22%), Space: 69.9 MB (42.53%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -5,11 +5,15 @@ class Solution:
         is_prime = [True for _ in range(n)]
         is_prime[0] = is_prime[1] = False
         i = 2
-        while i * i <= n:
+
+        
+        while i**2 <= n:
             if is_prime[i]:
                 j = i * i
                 while j < n:
                     is_prime[j] = False
                     j += i
             i += 1
-        return (sum(is_prime))
+            
+        return sum(is_prime)
+    
